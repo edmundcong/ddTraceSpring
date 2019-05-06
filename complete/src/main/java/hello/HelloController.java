67436fa836415f91fe9d727afedfb8fe1a7b16a5
edmundcong <edmundcong1@gmail.com>
2019-05-06 16:02:57 +1000

    got tracing working

--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -1,12 +1,27 @@
 package hello;
 
-import org.springframework.web.bind.annotation.RestController;
+import datadog.trace.api.DDTags;
+import datadog.trace.api.Trace;
+import io.opentracing.Span;
+import io.opentracing.util.GlobalTracer;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     @RequestMapping("/")
     public String index() {
+        myMethod();
         return "Greetings from our Java APM app!";
+    }
+
+    @Trace
+    void myMethod() {
+        final Span span = GlobalTracer.get().activeSpan();
+        // Span provided by @Trace annotation.
+        if (span != null) {
+            span.setTag(DDTags.SERVICE_NAME, "my-custom-service");
+        }
     }
 }
